0fe5bff4471f5c99fe0c95c8b575aa83a73401a0
WolfHaley0 <sergseif89@gmail.com>
2018-09-24 02:21:39 +0300

    chng

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
  */
 
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size = 0;
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
         Arrays.fill(storage, 0, size, null);
@@ -22,7 +22,7 @@ public class ArrayStorage {
 
     Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid) {
+            if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
         }
@@ -31,20 +31,21 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         for (int i = 0; i < size; i++) {
-
-            if (storage[i].uuid == uuid) {
+            if (storage[i].uuid.equals(uuid)) {
                 storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+                return;
             }
         }
-        size--;
     }
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] storage2 = new Resume[size];
         return Arrays.copyOf(storage, size);
     }
+
     int size() {
         return size;
     }
